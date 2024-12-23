56fa5b994186c90be57447b2fc961c2995311127
Your Name <you@example.com>
2024-12-23 05:07:10 +0300

    fix

--- scripts/insert_words.js
+++ scripts/insert_words.js
@@ -1,22 +1,17 @@
 import 'dotenv/config'
 import mongoose from "mongoose";
+import Word from "../bot/database/models/Word.js";
+import {HSK_1_JSON, HSK_2_JSON, HSK_3_JSON, HSK_4_JSON, HSK_5_JSON, HSK_6_JSON} from "../initial_data/words.js";
 
-const wordSchema = new mongoose.Schema({
-    word_id: Number,
-    ieroglif: String,
-    transcription: String,
-    translation: String,
-    audio_link: String,
-    ed_level: Number
-});
-
-
-const Word = mongoose.model('Word', wordSchema);
 
 async function main() {
     await mongoose.connect(process.env.DATABASE_URL)
-    const result = await Word.aggregate([{ $sample: { size: 1 } }]);
-    console.log(result)
+    await Word.insertMany(HSK_1_JSON)
+    await Word.insertMany(HSK_2_JSON)
+    await Word.insertMany(HSK_3_JSON)
+    await Word.insertMany(HSK_4_JSON)
+    await Word.insertMany(HSK_5_JSON)
+    await Word.insertMany(HSK_6_JSON)
 }
 
 
